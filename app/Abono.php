326745f2f8a431229d2a47636600d948717480a5
Rennerp <rekefa@gmail.com>
2020-01-20 22:46:09 -0600

    Abono Modelo

---
+++ app/Abono.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Abono extends Model
+{
+    protected $table = 'Abono';
+    protected $primaryKey = 'ID_Abono';
+    protected $timestamps = false;
+    protected $fillable = [
+        'Numero_Factura',
+        'Monto_Abonado',
+        'Descripcion_Abono',
+        'Fecha_Realizacion'
+    ];
+}
---
+++ app/Http/Controllers/AbonoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AbonoController extends Controller
+{
+    public function index(){}
+
+    public function create(){}
+
+    public function store(){}
+
+    public function edit(){}
+
+    public function update(){}
+
+}
