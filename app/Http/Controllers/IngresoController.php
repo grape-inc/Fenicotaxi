a55607ba3509caf18114077339ec7a71e57e21a4
Rennerp <rekefa@gmail.com>
2021-02-18 21:16:47 -0600

    Feature UpdateIngreso - Se realiza update para ingresos

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -63,7 +63,8 @@ class IngresoController extends Controller
             $detalles_a_guardar = [];
             $detalles = $request->get('ingreso_detalles');
             foreach ($detalles as $detalle) {
-                array_push($detalles_a_guardar,
+                array_push(
+                    $detalles_a_guardar,
                     new IngresoDetalle($detalle)
                 );
             };
@@ -92,19 +93,52 @@ class IngresoController extends Controller
             ->where('prod.Existencias_Minimas', '>', '0')
             ->get();
 
-        return view('inventario.Ingresos.edit',
-                   ["ingreso" => $ingreso,
-                    "empleado" => $empleado,
-                    "divisa" => $divisa,
-                    "proveedor" => $proveedor,
-                    "ingreso_detalle" => $detalles,
-                    "producto" => $producto]);
+        return view(
+            'inventario.Ingresos.edit',
+            [
+                "ingreso" => $ingreso,
+                "empleado" => $empleado,
+                "divisa" => $divisa,
+                "proveedor" => $proveedor,
+                "ingreso_detalle" => $detalles,
+                "producto" => $producto
+            ]
+        );
     }
 
     public function update(IngresosFormRequest $request)
     {
-        $ingreso_id = $request->ID_Ingreso;
-        $ingreso = Ingreso::find($ingreso_id);
+        try {
+            DB::beginTransaction();
+            $ingreso_id = $request->ID_Ingreso;
+            $ingreso_a_actualizar = Ingreso::find($ingreso_id);
+            $ingreso_a_actualizar->ID_Proveedor = $request->get('ID_Proveedor');
+            $ingreso_a_actualizar->ID_Empleado = $request->get('ID_Empleado');
+            $ingreso_a_actualizar->Fecha_Realizacion = date('Y-m-d H:i:s');
+            $ingreso_a_actualizar->Impuesto = $request->get('Impuesto');
+            $ingreso_a_actualizar->Total = $request->get('Total');
+            $ingreso_a_actualizar->Codigo_Ingreso = $request->get('Codigo_Ingreso');
+            $ingreso_a_actualizar->ID_Divisa = $request->get('ID_Divisa');
+            $ingreso_a_actualizar->update();
 
+            $ingreso_a_actualizar->ingreso_detalles()->delete();
+
+            $detalles_a_guardar = [];
+            $detalles = $request->get('ingreso_detalles');
+            foreach ($detalles as $detalle) {
+                array_push(
+                    $detalles_a_guardar,
+                    new IngresoDetalle($detalle)
+                );
+            };
+
+            $ingreso_a_actualizar->ingreso_detalles()->saveMany($detalles_a_guardar);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
+        return redirect()->action('IngresoController@index');
     }
 }
